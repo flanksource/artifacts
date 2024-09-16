2b4f36dd66ea89c9d0d070f8d0f38598679d33b1
Aditya Thebe <contact@adityathebe.com>
2024-09-16 20:01:48 +0545

    feat: start SMB server on CI

--- fs/smb_test.go
+++ fs/smb_test.go
@@ -1,22 +1,71 @@
 package fs
 
-// TODO: Setup SMB server on CI
-// func TestSMB(t *testing.T) {
-// 	password := os.Getenv("NAS_SMB_PASSWORD")
+import (
+	"testing"
 
-// 	fs, err := NewSMBFS("localhost", "445", "aditya", types.Authentication{
-// 		Username: types.EnvVar{ValueStatic: "aditya"},
-// 		Password: types.EnvVar{ValueStatic: password}})
-// 	if err != nil {
-// 		t.Fatalf(err.Error())
-// 	}
+	"github.com/flanksource/duty/types"
+)
 
-// 	files, err := fs.ReadDir("documents/**/*.pdf")
-// 	if err != nil {
-// 		t.Fatalf(err.Error())
-// 	}
+func TestSMB(t *testing.T) {
+	username := "example1"
+	password := "badpass"
+	share := "users"
 
-// 	for _, p := range files {
-// 		fmt.Printf("file: %s\n", p.FullPath())
-// 	}
-// }
+	fs, err := NewSMBFS("localhost", "445", share, types.Authentication{
+		Username: types.EnvVar{ValueStatic: username},
+		Password: types.EnvVar{ValueStatic: password}})
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	if err := populateSMB(fs); err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	{
+		files, err := fs.ReadDir("*.json")
+		if err != nil {
+			t.Fatalf("%v", err)
+		}
+
+		if len(files) != 2 {
+			t.Errorf("expected 2 files, got %d", len(files))
+		}
+	}
+
+	{
+		files, err := fs.ReadDir("*.yaml")
+		if err != nil {
+			t.Fatalf("%v", err)
+		}
+
+		if len(files) != 1 {
+			t.Errorf("expected 1 files, got %d", len(files))
+		}
+	}
+}
+
+func populateSMB(fs *smbFS) error {
+	testData := []struct {
+		name    string
+		content string
+	}{
+		{"first.json", "first"},
+		{"second.json", "second"},
+		{"third.yaml", "third"},
+	}
+
+	for _, td := range testData {
+		f, err := fs.Create(td.name)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		if _, err := f.Write([]byte(td.content)); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
